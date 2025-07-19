20e07f97b6aaee54c9f246e0a14316bad06bfa7e
Thomas <thomas.s.cordeiro@hotmail.com>
2025-07-19 14:52:51 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,10 +67,6 @@ data "aws_iam_policy_document" "s3_policy" {
   statement {
     actions = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.site_bucket.arn}/*"]
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
     condition {
       test     = "StringEquals"
       variable = "AWS:SourceArn"
